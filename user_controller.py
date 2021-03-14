d2ad60c19faf4c90bf8a57d03f74104b9f2e7fac
nickluong97@outlook.com <nickluong97@outlook.com>
2021-03-13 23:07:58 -0800

    Big user_controller fix. Used globals.. but it works now.

--- user_controller.py
+++ user_controller.py
@@ -1,25 +1,71 @@
+import discord
 import character_classes
 import random
 from discord.ext import commands
-AVAILABLE_CLASSES = [character_classes.Warrior,
-                     character_classes.Mage,
-                     character_classes.Brawler,
-                     character_classes.Priest,
-                     character_classes.DarkKnight,
-                     character_classes.Thief]
+
+AVAILABLE_CLASSES = {"\U00002694": character_classes.Warrior,
+                     "\U0001FA84": character_classes.Mage,
+                     "\U0001F94A": character_classes.Brawler,
+                     "\U0001F489": character_classes.Priest,
+                     "\U0001FAA6": character_classes.DarkKnight,
+                     "\U0001F5E1": character_classes.Thief}
+
+bot = commands.Bot(command_prefix='$')
+client = discord.Client()
+
+user_reacted = set()
+user_chosen_class = {}
 
 
 class user_controller(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
-    # @commands.command()
-    # async def get_stats(self, ctx):
-    #     await ctx.send(test_print())
+    user_reacted = set()
+
+    @commands.command()
+    async def start(self, ctx):
+        em = discord.Embed(title="Create Character",
+                           description=f"Choose a class for your first character {ctx.message.author.mention}.")
+        em.add_field(name="Warrior", value="\U00002694")
+        em.add_field(name="Mage", value="\U0001FA84")
+        em.add_field(name="Brawler", value="\U0001F94A")
+        em.add_field(name="Priest", value="\U0001F489")
+        em.add_field(name="DarkKnight", value="\U0001FAA6")
+        em.add_field(name="Thief", value="\U0001F5E1")
+        msg = await ctx.send(embed=em)
+        await msg.add_reaction("\U00002694")
+        await msg.add_reaction("\U0001FA84")
+        await msg.add_reaction("\U0001F94A")
+        await msg.add_reaction("\U0001F489")
+        await msg.add_reaction("\U0001FAA6")
+        await msg.add_reaction("\U0001F5E1")
+
+    @commands.Cog.listener()
+    async def on_raw_reaction_add(self, payload):
+        channel = await self.bot.fetch_channel(payload.channel_id)
+        message = await channel.fetch_message(payload.message_id)
+        user = await self.bot.fetch_user(payload.user_id)
+        emoji = payload.emoji
+
+        if (payload.user_id, payload.message_id) not in user_reacted and payload.user_id != 820355672587108422:
+            user_reacted.add((payload.user_id, payload.message_id))
+            user_chosen_class[payload.user_id] = AVAILABLE_CLASSES[emoji.name]()
+            em = discord.Embed(title="Your Stats",
+                               description=f"{payload.member.name}'s {user_chosen_class[payload.user_id].get_classname()} "
+                                           f"Stats")
+
+            em.add_field(name="HP: ", value=user_chosen_class[payload.user_id].get_hp())
+            em.add_field(name="MP: ", value=user_chosen_class[payload.user_id].get_mp())
+            em.add_field(name="Attack: ", value=user_chosen_class[payload.user_id].get_attack())
+            em.add_field(name="Magic Attack: ", value=user_chosen_class[payload.user_id].get_magic_attack())
+            em.add_field(name="Defense: ", value=user_chosen_class[payload.user_id].get_defense())
+            em.add_field(name="Resistance: ", value=user_chosen_class[payload.user_id].get_resistance())
+            em.add_field(name="Total Stats: ", value=user_chosen_class[payload.user_id].get_total())
+            em.add_field(name="Description: ", value=user_chosen_class[payload.user_id].get_description())
+            em.add_field(name="Rarity: ", value=user_chosen_class[payload.user_id].get_rarity())
+            await channel.send(embed=em)
 
 
 def setup(bot):
     bot.add_cog(user_controller(bot))
-
-
-print(random.choice(AVAILABLE_CLASSES)().get_class_stats())
